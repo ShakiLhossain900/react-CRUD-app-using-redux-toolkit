5b4304bd83f55586a87a0636719a88cc2e3abe75
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-16 10:25:53 +0600

    adding

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <Link to='/' className={classes.nav} >Home </Link>
+            <Link to='/show-book' className={classes.nav} >Show Book</Link>
+            <Link to='/add-book' className={classes.nav} >Add Book </Link>
+        </nav>
     );
 };
 
