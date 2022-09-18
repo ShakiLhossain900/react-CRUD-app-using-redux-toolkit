33e62c63f542598aba5acdb271a21b7e94a5de0a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-18 21:20:05 +0600

    problem is solved

--- src/features/books/BooksSlice.js
+++ src/features/books/BooksSlice.js
@@ -1,18 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit"; changes to the next line
+const { createSlice } = require("@reduxjs/toolkit");
+
 
 const initialBooks = {
   books: [
-    { id: 1, title: "History of SS BlockChainIT", author: "Gazi Shakil" },
-    { id: 2, title: "Love Bangladesh", author: "Shamima akter" },
+    { id: '1', title: "History of SS BlockChainIT", author: "Gazi Shakil" },
+    { id: '2', title: "Love Bangladesh", author: "Shamima akter" },
   ],
 };
+
 
 export const booksSlice = createSlice({
     name: "books", 
     initialState:initialBooks,
     reducers: {
         showBooks : (state) => state,  //reducer er vitor object akare ami akta akta kore object showBooks er vitor rakhbu and 
-    }
+    },
 
 })
 
--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const BooksView = () => {
-    const books = useSelector((state)=> state.booksReducer.books)
-    console.log(books);
-    return (
-        <div>
-            <h2>list of books Books</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-            </table>
-            <tbody>
-                { books & books.map((book)=>{
-                    const {id,title,author} = book;
-                    return <tr>
-                        <td>{id}</td>
-                        <td>{title}</td>
-                        <td>{author}</td>
-                    </tr>
-                
-                })}
-            </tbody>
-        </div>
-    );
+  const books = useSelector((state) => state.booksReducer.books);
+   console.log(books);
+  return (
+    <div>
+      <h2>List of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((book) => {
+              const { id, title, author } = book;
+              return <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{author}</td>
+                <td>
+                   <button >Edit</button> 
+                   <button >Delete</button> 
+                </td>
+              </tr>;
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BooksView;
